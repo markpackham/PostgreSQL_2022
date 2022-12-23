7e626bd7d660e1586f5530bdfa483d3762bb13ff
mark <markpackham1@gmail.com>
2022-12-23 11:36:49 +0000

    table creation
    https://youtu.be/85pG_pDkITY?t=423

---
+++ CreatedTable.sql
@@ -0,0 +1,13 @@
+-- Database: sales_db2
+
+-- DROP DATABASE IF EXISTS sales_db2;
+
+CREATE DATABASE sales_db2
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United Kingdom.1252'
+    LC_CTYPE = 'English_United Kingdom.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
