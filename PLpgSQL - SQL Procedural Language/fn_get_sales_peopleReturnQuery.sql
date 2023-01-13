f40ff95dd5e772f3b21666a0582483d79b742da0
mark <markpackham1@gmail.com>
2023-01-13 12:10:33 +0000

    demo returning a query in plpgsql
    https://youtu.be/85pG_pDkITY?t=8911

---
+++ PLpgSQL - SQL Procedural Language/fn_get_sales_peopleReturnQuery.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION fn_get_sales_people()
+-- notice use of RETURNS instead of just RETURN
+RETURNS SETOF sales_person AS
+$body$
+	BEGIN
+		-- RETURN the QUERY
+		RETURN QUERY
+		SELECT *
+		FROM sales_person;
+	END;
+$body$
+LANGUAGE plpgsql
+
+SELECT (fn_get_sales_people()).*;
